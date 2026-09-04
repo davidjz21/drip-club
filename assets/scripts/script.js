4e7a8b9c5f0474b2eb262bd5f5b1491f2e5cc63d
JuanD2056 <juand0606@rocketmail.com>
2026-09-04 13:58:12 -0500

    Agrego sección de categorías, script.js, Font Awesome y correcciones menores

---
+++ assets/scripts/script.js
@@ -0,0 +1,28 @@
+function mostrarCategorias(categoria) {
+    let subcategorias = document.getElementById("subcategorias");
+
+    if (categoria == "hombres") {
+        subcategorias.innerHTML = `<button class="btn btn-outline-dark">Polos</button>
+                                   <button class="btn btn-outline-dark">Pantalones</button>
+                                   <button class="btn btn-outline-dark">Zapatos</button>
+                                   <button class="btn btn-outline-dark">Zapatillas</button>
+                                   <button class="btn btn-outline-dark">Casacas</button>`;
+
+    } else if (categoria == "mujeres") {
+        subcategorias.innerHTML =`<button class="btn btn-outline-dark">Polos</button>
+                                  <button class="btn btn-outline-dark">Blusas</button>
+                                  <button class="btn btn-outline-dark">Pantalones</button>
+                                  <button class="btn btn-outline-dark">Faldas</button>
+                                  <button class="btn btn-outline-dark">Vestidos</button>
+                                  <button class="btn btn-outline-dark">Tacos</button>
+                                  <button class="btn btn-outline-dark">Zapatillas</button>
+                                  <button class="btn btn-outline-dark">Casacas</button>`;
+
+    } else if (categoria == "niños") {
+        subcategorias.innerHTML =`<button class="btn btn-outline-dark">Polos</button>
+                                  <button class="btn btn-outline-dark">Pantalones</button>
+                                  <button class="btn btn-outline-dark">Shorts</button>
+                                  <button class="btn btn-outline-dark">Zapatillas</button>
+                                  <button class="btn btn-outline-dark">Casacas</button>`;
+    }
+}
